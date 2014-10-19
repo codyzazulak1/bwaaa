3af25bea2aeda1d5a12ce42db7607022d14a21f5
codyzazulak1 <codyzazulak1@gmail.com>
2014-10-19 00:33:15 +0000

    added cookie counter

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 $(document).ready(function() {
-  var counter = 0
   var snd = new Audio("sample.mp3");
+  var counter = 0;
+  var value = parseInt($.cookie("bwa"));
+  
+  if ( isNaN(value) ) {
+    counter = 0;
+  } else {
+    counter = value;
+    $(".count").text("BWAAA x " + counter); 
+  }
 
   $(".bwa").click(function() {
     snd.play();
-    snd.currentTime=0
+    snd.currentTime=0;
     $(this).fadeOut(50);
     $(this).fadeIn(50);
-    counter += 1
-    console.log("BWAAA x " + counter)
-
+    counter += 1;
+    $.cookie('bwa', counter);
+    console.log("BWAAA x " + $.cookie("bwa") );
     $(".count").text("BWAAA x " + counter);
   });
 });
