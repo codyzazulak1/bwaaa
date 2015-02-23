d92264f41d33d0d5f37882bfd4400ac8d1b0e976
codyzazulak1 <codyzazulak1@gmail.com>
2015-02-23 02:07:50 +0000

    add random background gen

--- script.js
+++ script.js
@@ -2,13 +2,22 @@ $(document).ready(function() {
   var snd = new Audio("sample.mp3");
   var counter = 0;
   var cook = parseInt($.cookie("bwa"));
+  var randomNum = Math.floor((Math.random() * 3) + 1);
+
+  if (randomNum == 1) {
+    $('body').css('background-image', 'url(bg.jpg )');
+  } else if (randomNum == 2) {
+    $('body').css('background-image', 'url(bg2.jpg)');
+  } else {
+    $('body').css('background-image', 'url(bg3.jpg)');
+  }
   
   if ( isNaN(cook) ) {
     counter = 0;
-    $(".count").text("BWAAA x " + counter);
+    $(".count").text("# # BWAAA x " + counter + " # #");
   } else {
     counter = cook;
-    $(".count").text("BWAAA x " + counter); 
+    $(".count").text("# # BWAAA x " + counter + " # #");
   }
 
   $(".bwa").click(function() {
@@ -20,12 +29,13 @@ $(document).ready(function() {
     $(this).fadeIn(50);
     counter += 1;
     $.cookie('bwa', counter);
-    $(".count").text("BWAAA x " + counter);
+    $(".count").text("# # BWAAA x " + counter + " # #");
   });
 
   $(".reset").click(function() {
     counter = 0;
     $.cookie('bwa', counter);
-    $(".count").text("BWAAA x " + counter);
+    $(".count").text("# # BWAAA x " + counter + " # #");
   });
+
 });
